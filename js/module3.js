5393ef3fb6e453cb7a123b756bc996f1d05c1a43
ZhanTianci <2154325737@qq.com>
2023-04-03 19:56:17 +0800

    添加icon

--- js/module3.js
+++ js/module3.js
@@ -1,15 +1,22 @@
-// const titleCards = document.querySelector('.title-cards');
+//每一个大标题栏
 const titleBoxs = document.querySelectorAll('.title-box');
+// 大标题栏下的h4标签
+const h4 = document.querySelectorAll('.title-h4-icon');
+// 大标题栏下的p标签
 const titleTexts = document.querySelectorAll('.title-text');
+// 右侧图片
 const imgs = document.querySelectorAll('.imgs');
 
+// 给每个大标题栏添加点击事件
 for(let i = 0; i <= titleBoxs.length - 1; i++) {
   titleBoxs[i].addEventListener('click',function() {
     document.querySelector('.focus-box').classList.remove('focus-box');
+    document.querySelector('.icon-xiasanjiao').classList.replace('icon-xiasanjiao','icon-yousanjiao');
     this.classList.add('focus-box');
     for(let j = 0; j <= titleTexts.length - 1; j++) {
       titleTexts[j].style.display = 'none';
     }
+    h4[i].classList.replace('icon-yousanjiao','icon-xiasanjiao');
     titleTexts[i].style.display = 'block';
     document.querySelector('.focus-img').classList.remove('focus-img');
     imgs[i].classList.add('focus-img');
--- js/slider.js
+++ js/slider.js
@@ -8,10 +8,16 @@ control.addEventListener('click',function() {
   if(count === 1) {
     slider.classList.add('go-out');
     main.style.paddingLeft = '0';
+    this.innerHTML = '&#xe624;';
+    this.classList.add('appear');
+    this.classList.remove('hidden');
   } 
   else if (count === 2) {
     slider.classList.remove('go-out');
-    main.style.paddingLeft = '160px'
+    main.style.paddingLeft = '160px';
+    this.innerHTML = '&#xe622;';
+    this.classList.remove('appear');
+    this.classList.add('hidden');
   }
   count = 3 - count;
 })
